320fc04f5d173f8121cccb7ef6fd4586ebd38b62
handong <handong5@gt.cn>
2025-05-13 09:59:03 +0800

    aaa

---
+++ src/test/java/com/mulfarsh/dhj/basaldb/BasalDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mulfarsh.dhj.basaldb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasalDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
